5b8a3e44a1eb5c596de56270ddc10f39f30e8bd3
Daito Manabe <daito@rhizomatiks.com>
2016-10-30 18:02:15 +0900

    add drawImagesExample.

---
+++ drawImagesExample/src/ofApp.cpp
@@ -0,0 +1,81 @@
+#include "ofApp.h"
+#include "ofxTinySingleton.h"
+
+class Image{
+public:
+    ofImage img;
+    Image(){
+        cout << "load image file"<<endl;
+        img.load("dog.jpg");
+    }
+    void draw(int x, int y){
+        img.draw(x,y);
+    }
+    ~Image(){
+        cout << "Image is being deleted" << endl;
+    }
+};
+//--------------------------------------------------------------
+void ofApp::draw(){
+    ofSetWindowTitle(ofToString(ofGetFrameRate()));
+    for(int x=0;x<40;x++){
+        for(int y=0;y<40;y++){
+            ofxTinySingleton<Image>::get_instance().draw(x*200, y*200);
+        }
+    }
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
